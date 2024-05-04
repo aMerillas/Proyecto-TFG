1ea4bc61162c1a5bff0f6acb04c95a633677668d
Carlota <carlota.cdb221@gmail.com>
2024-05-04 13:51:16 +0200

    Se muestran los recyclers correspondientes a la lista de favoritos y guardados

--- app/src/main/java/com/example/proyectotfg/MainView.java
+++ app/src/main/java/com/example/proyectotfg/MainView.java
@@ -2,8 +2,13 @@ package com.example.proyectotfg;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,19 +16,29 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.proyectotfg.recyclerView.SpotsAdapterFB;
 import com.example.proyectotfg.recyclerView.SpotsFB;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+
+import java.util.List;
 
 public class MainView extends AppCompatActivity {
 
     FirebaseFirestore firebaseFirestore;
     private SpotsAdapterFB mSpotsAdapterFB;
     private RecyclerView mRecyclerView;
+    private ImageView imageView;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainview);
+        imageView = findViewById(R.id.not_found);
         // Instanciamos el Firebase
         firebaseFirestore = FirebaseFirestore.getInstance();
         // Inicializa el RecyclerView
@@ -33,7 +48,6 @@ public class MainView extends AppCompatActivity {
         FirestoreRecyclerOptions<SpotsFB> firestoreRecyclerOptions =
                 new FirestoreRecyclerOptions.Builder<SpotsFB>().setQuery(query, SpotsFB.class).build();
         mSpotsAdapterFB = new SpotsAdapterFB(firestoreRecyclerOptions);
-        mSpotsAdapterFB.notifyDataSetChanged();
         mSpotsAdapterFB.startListening();
         mSpotsAdapterFB.setOnItemClickListener(new SpotsAdapterFB.OnItemClickListener() {
             @Override
@@ -49,13 +63,105 @@ public class MainView extends AppCompatActivity {
         });
         mRecyclerView.setAdapter(mSpotsAdapterFB);
 
-        Button myFavSpotsButton = findViewById(R.id.myFavSpotsButtonMainView);
-        Button myNextSpotsButton = findViewById(R.id.myNextSpotsButtonMainView);
-        Button spotsListButton = findViewById(R.id.spotsListButtonMainView);
+        //Nav
+        BottomNavigationView mybottomNavView = findViewById(R.id.bottom_navigation);
+        mybottomNavView.getMenu().getItem(1).setChecked(true);
+        mybottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.spots_Next) {
+                    item.setChecked(true);
+                    Toast.makeText(MainView.this, "Next", Toast.LENGTH_SHORT).show();
+                    nextSpots();
+                } else if (item.getItemId() == R.id.spots_List) {
+                    item.setChecked(true);
+                    Toast.makeText(MainView.this, "List", Toast.LENGTH_SHORT).show();
+                    listSpots();
+                } else if (item.getItemId() == R.id.spots_Favs) {
+                    item.setChecked(true);
+                    Toast.makeText(MainView.this, "Favs", Toast.LENGTH_SHORT).show();
+                    favSpots();
+                }
+                return false;
+            }
+        });
 
-        myFavSpotsButton.setOnClickListener(view -> Navigator.openActivity(MainView.this, MyFavSpots.class));
-        myNextSpotsButton.setOnClickListener(view -> Navigator.openActivity(MainView.this, MyNextSpots.class));
-        spotsListButton.setOnClickListener(view -> Navigator.openActivity(MainView.this, SpotsList.class));
+    }
+
+    private void nextSpots() {
+        // Obtener la referencia del usuario actualmente autenticado
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+            // Obtener una referencia al documento del usuario
+            DocumentReference userRef = firebaseFirestore.collection("user").document(userId);
+            // Obtener el array de IDs de spots del documento del usuario
+            userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if (documentSnapshot.exists()) {
+                        // Obtener el array de IDs de spots del documento del usuario
+                        List<Integer> spotIDs = (List<Integer>) documentSnapshot.get("nextSpots");
+                        if (spotIDs != null && !spotIDs.isEmpty()) { // Verificar si el array no está vacío
+                            // Filtrar los spots basados en los IDs obtenidos
+                            Query query = firebaseFirestore.collection("spots").whereIn("id", spotIDs);
+                            FirestoreRecyclerOptions<SpotsFB> firestoreRecyclerOptions =
+                                    new FirestoreRecyclerOptions.Builder<SpotsFB>().setQuery(query, SpotsFB.class).build();
+                            mSpotsAdapterFB.updateOptions(firestoreRecyclerOptions);
+                            mRecyclerView.setAdapter(mSpotsAdapterFB); // Establece el adaptador en el RecyclerView
+                        } else {
+                            // El array de IDs de spots está vacío
+                            mRecyclerView.setVisibility(View.INVISIBLE);
+                            imageView.setVisibility(View.VISIBLE);
+                            Toast.makeText(MainView.this, "No hay spots guardados", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    private void listSpots() {
+        Query query = firebaseFirestore.collection("spots");
+        FirestoreRecyclerOptions<SpotsFB> firestoreRecyclerOptions =
+                new FirestoreRecyclerOptions.Builder<SpotsFB>().setQuery(query, SpotsFB.class).build();
+        mSpotsAdapterFB.updateOptions(firestoreRecyclerOptions);
+        mRecyclerView.setAdapter(mSpotsAdapterFB);
+        imageView.setVisibility(View.INVISIBLE);
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void favSpots() {
+        // Obtener la referencia del usuario actualmente autenticado
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+            // Obtener una referencia al documento del usuario
+            DocumentReference userRef = firebaseFirestore.collection("user").document(userId);
+            // Obtener el array de IDs de spots del documento del usuario
+            userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if (documentSnapshot.exists()) {
+                        // Obtener el array de IDs de spots del documento del usuario
+                        List<Integer> spotIDs = (List<Integer>) documentSnapshot.get("favSpots");
+                        if (spotIDs != null && !spotIDs.isEmpty()) { // Verificar si el array no está vacío
+                            // Filtrar los spots basados en los IDs obtenidos
+                            Query query = firebaseFirestore.collection("spots").whereIn("id", spotIDs);
+                            FirestoreRecyclerOptions<SpotsFB> firestoreRecyclerOptions =
+                                    new FirestoreRecyclerOptions.Builder<SpotsFB>().setQuery(query, SpotsFB.class).build();
+                            mSpotsAdapterFB.updateOptions(firestoreRecyclerOptions);
+                            mRecyclerView.setAdapter(mSpotsAdapterFB); // Establece el adaptador en el RecyclerView
+                        } else {
+                            // El array de IDs de spots está vacío
+                            mRecyclerView.setVisibility(View.INVISIBLE);
+                            imageView.setVisibility(View.VISIBLE);
+                            Toast.makeText(MainView.this, "No hay spots favoritos", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            });
+        }
     }
 
 }
