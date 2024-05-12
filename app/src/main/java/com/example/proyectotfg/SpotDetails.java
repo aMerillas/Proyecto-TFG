0e9eed599c5d27324eadeb903552bb84a047ebaf
Carlota <carlota.cdb221@gmail.com>
2024-05-12 13:01:54 +0200

    Minor changes

--- app/src/main/java/com/example/proyectotfg/SpotDetails.java
+++ app/src/main/java/com/example/proyectotfg/SpotDetails.java
@@ -1,13 +1,10 @@
 package com.example.proyectotfg;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
