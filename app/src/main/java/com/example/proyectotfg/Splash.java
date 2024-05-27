6887fb3aff90c1b141e5e63cbdec8b5f5f4ed220
Carlota <carlota.cdb221@gmail.com>
2024-05-27 10:35:28 +0200

    Correccion inicio app

--- app/src/main/java/com/example/proyectotfg/Splash.java
+++ app/src/main/java/com/example/proyectotfg/Splash.java
@@ -1,7 +1,9 @@
 package com.example.proyectotfg;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,7 +13,20 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashview);
+        openApp();
+    }
 
+    private void openApp() {
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splash.this, Login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        },4000);
     }
 
 
