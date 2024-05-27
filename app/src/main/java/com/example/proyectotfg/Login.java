f83ae03ee0b849488dc49c82b9b4e4164066879e
Alvarito03 <alvaroguadalupe02@gmail.com>
2024-05-27 09:19:47 +0200

    Cambios login register

--- app/src/main/java/com/example/proyectotfg/Login.java
+++ app/src/main/java/com/example/proyectotfg/Login.java
@@ -1,14 +1,18 @@
 package com.example.proyectotfg;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
--- app/src/main/java/com/example/proyectotfg/Register.java
+++ app/src/main/java/com/example/proyectotfg/Register.java
@@ -1,14 +1,18 @@
 package com.example.proyectotfg;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -39,6 +43,7 @@ public class Register extends AppCompatActivity {
         userPass = findViewById(R.id.password);
         userPassRep = findViewById(R.id.passwordconfirm);
         Button registerButton = findViewById(R.id.registerButtonRegisterView);
+
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
